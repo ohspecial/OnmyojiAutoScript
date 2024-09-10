61114a962f15d2ddcd23982cf1495332f214049b
jack <q459278554@gmail.com>
2024-09-10 20:03:55 +0800

    feat(HeroTest):增加更换御魂

--- tasks/HeroTest/config.py
+++ tasks/HeroTest/config.py
@@ -6,6 +6,7 @@ from datetime import datetime, time  # type: ignore
 from pydantic import BaseModel, Field
 
 from tasks.Component.GeneralBattle.config_general_battle import GeneralBattleConfig
+from tasks.Component.SwitchSoul.switch_soul_config import SwitchSoulConfig
 from tasks.Component.config_scheduler import Scheduler
 from tasks.Component.config_base import ConfigBase, Time, TimeDelta
 from tasks.Component.BaseActivity.config_activity import GeneralClimb
@@ -37,3 +38,5 @@ class HeroTest(ConfigBase):
     scheduler: Scheduler = Field(default_factory=Scheduler)
     herotest: HeroTestConfig = Field(default_factory=HeroTestConfig)
     general_battle: GeneralBattleConfig = Field(default_factory=GeneralBattleConfig)
+    switch_soul_config: SwitchSoulConfig = Field(default_factory=SwitchSoulConfig)
+
