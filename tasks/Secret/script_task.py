833ddfbda7e48c7a26cf397f6d04b3c1a1a2a8ac
jack <q59278554@gmail.com>
2025-05-26 18:33:37 +0800

    fix(Secret)：修复OCR识别错误后的退出逻辑

--- tasks/Secret/script_task.py
+++ tasks/Secret/script_task.py
@@ -84,10 +84,11 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, SecretAssets):
                     logger.warning('You have completed the weekly trifles, skip')
                     break
                 text = self.O_SE_TOTAL_TIME.ocr_single(self.device.image)
-                if '总耗时' in text:
+                if '尚未' not in text:
                     logger.warning('You have completed the weekly trifles, skip')
                     break
                 continue
+            
             if layer >= 6:
                 first_battle = False
             if first_battle and layer <= 5:
