ad2dda21de69af0791994a72556a48c1dd4118ee
jack <q59278554@gmail.com>
2025-07-21 22:18:03 +0800

    save duel

--- tasks/Duel/script_task.py
+++ tasks/Duel/script_task.py
@@ -20,30 +20,8 @@ from tasks.GameUi.page import page_main, page_team, page_shikigami_records
 """ 斗技 """
 
 
-class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
-    battle_count = 0
-    battle_win_count = 0
-    battle_lose_count = 0
+class ScriptTask(GameUi, GeneralBattle, DuelAssets):
     def run(self):
-
-        current_time = datetime.now().time()
-        if not (time(12, 00) <= current_time < time(23, 00)):
-            logger.warning('不在斗技时间段')
-            self.set_next_run(task='Duel', success=True, finish=False)
-            raise TaskEnd('Duel')
-
-        con = self.config.duel
-        # 切换御魂
-        if con.switch_soul.enable:
-            self.ui_get_current_page()
-            self.ui_goto(page_shikigami_records)
-            self.run_switch_soul(con.switch_soul.switch_group_team)
-
-        if con.switch_soul.enable_switch_by_name:
-            self.ui_get_current_page()
-            self.ui_goto(page_shikigami_records)
-            self.run_switch_soul_by_name(con.switch_soul.group_name,con.switch_soul.team_name)
-
         con = self.config.duel.duel_config
         limit_time = con.limit_time
         self.limit_time: timedelta = timedelta(hours=limit_time.hour, minutes=limit_time.minute,
@@ -71,20 +49,16 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
 
         self.ui_get_current_page()
         self.ui_goto(page_duel)
-        # 切换御魂
         if con.switch_all_soul:
             self.switch_all_soul()
 
         # 循环
-        duel_week_over = False
         while 1:
             self.screenshot()
             if self.appear_then_click(self.I_REWARD, interval=0.6):
                 continue
             if not self.duel_main():
                 continue
-            # 检查分数
-            current_score = self.check_score()
 
             if datetime.now() - self.start_time >= self.limit_time:
                 # 任务执行时间超过限制时间，退出
@@ -95,37 +69,15 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
                 current_score = 3000
                 duel_week_over = True
                 break
-
-            # if con.honor_full_exit and self.check_honor():
-            #     # 荣誉满了，退出
-            #     logger.info('Duel task is over honor')
-            #     break
-            # 当前分数跟目标分数比较
-            if current_score >= con.target_score:
-                # 分数够了
+            current_score = self.check_score(con.target_score)
+            if not current_score:
+                # 分数够了，退出
                 logger.info('Duel task is over score')
-                # 是否刷满荣誉就退出
-                if con.honor_full_exit:
-                    if self.check_honor():
-                        # 荣誉满了，退出
-                        # self.save_image(content=f'分数: {current_score}, 本周斗技结束', push_flag=True)
-                        logger.info('Duel task is over honor')
-                        duel_week_over = True
-                        break
-                else:
-                    break
-
-            # 进行一次斗技
+                break
             self.duel_one(current_score, con.green_enable, con.green_mark)
 
-        logger.info('Duel battle end')
-        self.config.notifier.push(title='斗技结束', content=f'本周斗技结束, 场次: {self.battle_count} | 胜: {self.battle_win_count} 败: {self.battle_lose_count} | 分数: {current_score}')
         # 记得退回去到町中
         self.ui_click(self.I_UI_BACK_YELLOW, self.I_CHECK_TOWN)
-
-        if duel_week_over:
-            self.set_next_run(task='Duel', success=True, finish=False)
-
         # 调起花合战
         self.set_next_run(task='TalismanPass', target=datetime.now())
         raise TaskEnd('Duel')
@@ -234,7 +186,7 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
             return True
         return False
 
-    def check_score(self) -> int or None:
+    def check_score(self, target: int) -> int or None:
         """
         检查是否达到目标分数
         :param target: 目标分数
@@ -242,15 +194,26 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
         """
         while 1:
             self.screenshot()
+            if self.appear(self.I_D_CELEB_STAR) or self.appear(self.I_D_CELEB_HONOR):
+                logger.info('You are already a celeb')
+                return None
             current_score = self.O_D_SCORE.ocr(self.device.image)
-            if current_score > 10000:
+            if current_score < 1200:
+                # 分太低了
+                logger.warning('Score is too low')
+                logger.error('Please enhance your score')
+                raise RequestHumanTakeover
+            elif current_score > 10000:
                 # 识别错误分数超过一万, 去掉最高位
                 logger.warning('Recognition error, score is too high')
+                logger.warning('Remove the highest digit')
                 current_score = int(str(current_score)[1:])
-            return current_score
+            elif current_score > 3000:
+                continue
+            return current_score if current_score <= target else None
 
-    def duel_one(self, current_score: int, enable: bool = False,
-                 mark_mode: GreenMarkType = GreenMarkType.GREEN_MAIN) -> bool:
+    def duel_one(self, current_score: int, enable: bool=False,
+                 mark_mode: GreenMarkType=GreenMarkType.GREEN_MAIN) -> bool:
         """
         进行一次斗技， 返回输赢结果
         :param mark_mode:
@@ -258,8 +221,6 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
         :param current_score: 当前分数, 不同的分数有不同的战斗界面
         :return:
         """
-        logger.hr('Duel battle', 2)
-        self.battle_count += 1
         while 1:
             self.screenshot()
             # if not self.appear(self.I_D_HELP):
@@ -291,18 +252,10 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
             if self.appear(self.I_D_PREPARE):
                 # 低段位有的准备
                 self.ui_click_until_disappear(self.I_D_PREPARE)
-                self.wait_until_disappear(self.I_D_PREPARE_DONE)
                 logger.info('Duel prepare')
                 break
         # 正式进入战斗
         logger.info('Duel start battle')
-        timer = Timer(10)
-        timer.start()
-        while 1:
-            if timer.reached():
-                break
-            if self.is_in_battle():
-                break
         while 1:
             self.screenshot()
             if self.ocr_appear(self.O_D_AUTO, interval=0.4):
@@ -311,13 +264,11 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
                 continue
             # 如果对方直接秒退，那自己就是赢的
             if self.appear(self.I_D_VICTORY):
+                logger.info('Duel battle win')
                 self.ui_click_until_disappear(self.I_D_VICTORY)
-                self.battle_win_count += 1
                 return True
         # 绿标
-        if enable:
-            if not self.duel_green_mark_1():
-                self.duel_green_mark(mark_mode)
+        self.green_mark(enable, mark_mode)
         # 等待结果
         logger.info('Duel wait result')
         self.device.stuck_record_add('BATTLE_STATUS_S')
@@ -332,21 +283,25 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
                 continue
             if self.appear(self.I_FALSE):
                 # 打输了
+                logger.info('Duel battle lose')
                 self.ui_click_until_disappear(self.I_FALSE)
                 battle_win = False
                 break
             if self.appear(self.I_D_FAIL):
                 # 输了
+                logger.info('Duel battle lose')
                 self.ui_click_until_disappear(self.I_D_FAIL)
                 battle_win = False
                 break
             if self.appear(self.I_WIN):
                 # 打赢了
+                logger.info('Duel battle win')
                 self.ui_click_until_disappear(self.I_WIN)
                 battle_win = True
                 break
             if self.appear(self.I_D_VICTORY):
                 # 打赢了
+                logger.info('Duel battle win')
                 self.ui_click_until_disappear(self.I_D_VICTORY)
                 battle_win = True
                 break
@@ -363,130 +318,10 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets):
                 self.device.stuck_record_clear()
                 self.device.stuck_record_add('BATTLE_STATUS_S')
 
-        if battle_win:
-            self.battle_win_count += 1
-        else:
-            self.battle_lose_count += 1
 
-        task_run_time = datetime.now() - self.start_time
-        # 格式化时间，只保留整数部分的秒
-        task_run_time_seconds = timedelta(seconds=int(task_run_time.total_seconds()))
-
-        logger.info(f'战斗结果: {battle_win}')
-        logger.info(f'战斗次数: {self.battle_count} | 胜利: {self.battle_win_count} 失败: {self.battle_lose_count}')
-        logger.info(f'战斗用时: {task_run_time_seconds} / {self.limit_time}')
         return battle_win
 
-    def duel_green_mark_1(self) -> bool:
-        logger.info('------进行图片匹配识别绿标位置------')
-        # 点击绿标
-        mark_timer = Timer(5)
-        mark_timer.start()
-        while 1:
-            if mark_timer.reached():
-                logger.info('Duel green mark timeout, dont appear I_GREEN_MARK_IMG')
-                return False
-            self.screenshot()
-            if self.appear(self.I_GREEN_MARK_IMG, interval=0.5):
-                new_roi_front = (self.I_GREEN_MARK_IMG.roi_front[0],
-                                 self.I_GREEN_MARK_IMG.roi_front[1] + 60,
-                                 10,
-                                 100)
-                self.C_DUEL_GREEN_LEFT_FULL.roi_front = new_roi_front
-                logger.info(f'old Image roi {self.I_GREEN_MARK_IMG.roi_front}')
-                logger.info(f'new Image roi {self.C_DUEL_GREEN_LEFT_FULL.roi_front}')
-                break
-        # 点击绿标
-        mark_timer = Timer(5)
-        mark_timer.start()
-        while 1:
-            if mark_timer.reached():
-                logger.info('Duel green mark timeout')
-                # self.save_image(wait_time=0, push_flag=True, content='超时未识别到绿标',image_type=True)
-                return False
-            self.screenshot()
-            if self.wait_until_appear(self.I_GREEN_MARK, self.I_GREEN_MARK_1, wait_time=1):
-                # self.save_image(wait_time=0, push_flag=True, content='识别到绿标',image_type=True)
-                return True
-            self.click(self.C_DUEL_GREEN_LEFT_FULL)
 
-    def wait_until_appear(self,
-                          target,
-                          target2,
-                          skip_first_screenshot=False,
-                          wait_time: int = None) -> bool:
-        """
-        等待直到出现目标
-        :param wait_time: 等待时间，单位秒
-        :param target:
-        :param skip_first_screenshot:
-        :return:
-        """
-        wait_timer = None
-        if wait_time:
-            wait_timer = Timer(wait_time)
-            wait_timer.start()
-        while 1:
-            if skip_first_screenshot:
-                skip_first_screenshot = False
-            else:
-                self.screenshot()
-            if wait_timer and wait_timer.reached():
-                return False
-            if self.appear(target) or self.appear(target2):
-                return True
-    def duel_green_mark(self, mark_mode: GreenMarkType = GreenMarkType.GREEN_MAIN):
-        """
-        绿标， 如果不使能就直接返回
-        :param enable:
-        :param mark_mode:
-        :return:
-        """
-        logger.info('------进行区域点击识别绿标位置------')
-        if self.wait_until_appear(self.I_GREEN_MARK, wait_time=1):
-            # logger.info("识别到绿标，返回")
-            return
-        # logger.info("Green is enable")
-        x, y = None, None
-        match mark_mode:
-            case GreenMarkType.GREEN_LEFT1:
-                x, y = self.C_DUEL_GREEN_LEFT_1.coord()
-                logger.info("Green left 1")
-            case GreenMarkType.GREEN_LEFT2:
-                x, y = self.C_GREEN_LEFT_2.coord()
-                logger.info("Green left 2")
-            case GreenMarkType.GREEN_LEFT3:
-                x, y = self.C_GREEN_LEFT_3.coord()
-                logger.info("Green left 3")
-            case GreenMarkType.GREEN_LEFT4:
-                x, y = self.C_GREEN_LEFT_4.coord()
-                logger.info("Green left 4")
-            case GreenMarkType.GREEN_LEFT5:
-                x, y = self.C_DUEL_GREEN_LEFT_5.coord()
-                logger.info("Green left 5")
-            case GreenMarkType.GREEN_MAIN:
-                x, y = self.C_GREEN_MAIN.coord()
-                logger.info("Green main")
-
-        # 等待那个准备的消失
-        while 1:
-            self.screenshot()
-            if not self.appear(self.I_PREPARE_HIGHLIGHT):
-                break
-
-        # 点击绿标
-        mark_timer = Timer(5)
-        mark_timer.start()
-        while 1:
-            self.screenshot()
-            if self.wait_until_appear(self.I_GREEN_MARK, wait_time=1):
-                # logger.info("识别到绿标,返回")
-                break
-            if mark_timer.reached():
-                # logger.warning("识别绿标超时,返回")
-                break
-            # 点击绿标
-            self.device.click(x, y)
 
 
 if __name__ == '__main__':
