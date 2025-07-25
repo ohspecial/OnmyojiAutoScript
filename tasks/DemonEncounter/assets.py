a4853e18df23fc900abd79ffc0ac269f13fb8dd4
jack <q59278554@gmail.com>
2025-07-25 22:41:42 +0800

    修复逢魔识别范围太大

--- tasks/DemonEncounter/assets.py
+++ tasks/DemonEncounter/assets.py
@@ -120,7 +120,7 @@ class DemonEncounterAssets:
 
 	# Ocr Rule Assets
 	# 计数已经开启多少的 
-	O_DE_COUNTER = RuleOcr(roi=(1204,685,48,34), area=(1204,685,48,34), mode="DigitCounter", method="Default", keyword="", name="de_counter")
+	O_DE_COUNTER = RuleOcr(roi=(1207,685,45,34), area=(1207,685,45,34), mode="DigitCounter", method="Default", keyword="", name="de_counter")
 
 
 	# Click Rule Assets
