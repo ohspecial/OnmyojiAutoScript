9ccb04a95f347f2cca2bdcada960afc273bd7488
ohspecial <q459278554@gmail.com>
2025-08-04 18:47:50 +0800

    fix(summon.py): 修正票数信息处理逻辑，适配OCR

--- tasks/Component/Summon/summon.py
+++ tasks/Component/Summon/summon.py
@@ -44,8 +44,10 @@ class Summon(BaseTask, SummonAssets):
             # 处理 None 和空字符串
             if ticket_info is None or ticket_info == '':
                 ticket_info = 0
+            elif isinstance(ticket_info, list):
+                ticket_info = ticket_info[1]
             else:
-                # 使用正则表达式提取字符串中的数字
+                # 使用正则表达式提取字符串中的数字   ['免费', '21/20']   
                 match = re.search(r'\d+', ticket_info)
                 if match:
                     ticket_info = int(match.group())
