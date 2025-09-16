9ed143acdd88457afbc12319f7995f504a6c7770
jack <q59278554@gmail.com>
2025-09-16 22:14:36 +0800

    fix(tasks/Secret): 修正金币识别区域并简化代码

--- tasks/Secret/script_task.py
+++ tasks/Secret/script_task.py
@@ -183,12 +183,7 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, SecretAssets):
             # 调整识别范围
             ocr_target.roi[0] = int(roi[0]) - 115
             ocr_target.roi[1] = int(roi[1]) + 37
-            x, y, w, h = ocr_target.roi
-            roi_image = self.device.image[y:y+h, x:x+w]
-            # cv2.imwrite('layer.png', roi_image)
-            # print(f'检测到的未通过ROI: {roi}')
-            # print(f'检测到的勾玉数量ROI: {ocr_target.roi}')
-            jade_num = ocr_target.ocr(roi_image)
+            jade_num = ocr_target.ocr(self.device.image)
             if isinstance(jade_num, str):
                 logger.warning(f'OCR failed, try again {jade_num}')
                 return None
@@ -303,7 +298,7 @@ if __name__ == '__main__':
     from module.config.config import Config
     from module.device.device import Device
 
-    c = Config('zhu')
+    c = Config('xiaohao')
     d = Device(c)
     t = ScriptTask(c, d)
     t.screenshot()
