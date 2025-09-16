0f7c153732153574a0e56e46a8a057ee81b5c577
jack <q59278554@gmail.com>
2025-09-16 22:17:22 +0800

    fix(tasks/AreaBoss/script_task.py): 移除不必要的图像保存功能
    移除了 `start_fight` 方法中调用的 `self.save_image()` 函数，该函数在每次战斗开始时保存图像，但由于该功能在当前实现中是不必要的，因此决定移除以简化代码。
    
    Closes #<相关issue编号>

--- tasks/AreaBoss/script_task.py
+++ tasks/AreaBoss/script_task.py
@@ -144,7 +144,6 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AreaBossAssets):
         return result
 
     def start_fight(self) -> bool:
-        self.save_image()
         while 1:
             self.screenshot()
             if self.appear_then_click(self.I_FIRE, interval=1):
