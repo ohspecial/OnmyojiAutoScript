52920781e6d08531a97369b0b5fec5cdeedf5c4a
Azure <bluetian1st@gmail.com>
2025-09-18 17:21:23 +0800

    add: boss climb act, buff switch refactor: climb config act fix: base task bugs

--- tasks/HeroTest/config.py
+++ tasks/HeroTest/config.py
@@ -9,7 +9,6 @@ from tasks.Component.GeneralBattle.config_general_battle import GeneralBattleCon
 from tasks.Component.SwitchSoul.switch_soul_config import SwitchSoulConfig
 from tasks.Component.config_scheduler import Scheduler
 from tasks.Component.config_base import ConfigBase, Time, TimeDelta
-from tasks.Component.BaseActivity.config_activity import GeneralClimb
 from tasks.Component.SwitchSoul.switch_soul_config import SwitchSoulConfig
 
 class Layer(str, Enum):
