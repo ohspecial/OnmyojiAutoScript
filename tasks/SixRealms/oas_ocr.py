1573c516f72284c9d5516720788e07fe33a080de
stigma <1461759570@qq.com>
2025-09-27 21:41:29 +0800

    fix(SixRealms): 修复在六道之门中，ocrServer报错问题

--- tasks/SixRealms/oas_ocr.py
+++ tasks/SixRealms/oas_ocr.py
@@ -1,15 +1,7 @@
-import functools
-
 import cv2
 import numpy as np
-
-
 from module.base.utils import color_similarity_2d, load_image
-# from module.ocr.ocr import Ocr
 from module.ocr.base_ocr import BaseCor
-
-
-
 
 def apply_mask(image, mask):
     image16 = image.astype(np.uint16)
@@ -22,42 +14,7 @@ def apply_mask(image, mask):
     # Image.fromarray(image16.astype(np.uint8)).show()
     return image16.astype(np.uint8)
 
-
-class VerticalText(BaseCor):
-    @staticmethod
-    def rotate_image(image):
-        height, width = image.shape[0:2]
-        if height * 1.0 / width >= 1.5:
-            image = np.rot90(image)
-            return image
-        return image
-
-    def detect_and_ocr(self, *args, **kwargs):
-        # Patch text_recognizer
-        text_recognizer = self.model.text_recognizer
-        # Lower drop_score
-        lower_score = functools.partial(self.model.detect_and_ocr)
-        detect_and_ocr = self.model.detect_and_ocr
-
-        def vertical_text_recognizer(img_crop_list):
-            img_crop_list = [VerticalText.rotate_image(i) for i in img_crop_list]
-            result = text_recognizer(img_crop_list)
-            return result
-
-
-        self.model.text_recognizer = vertical_text_recognizer
-        self.model.detect_and_ocr = lower_score
-
-        try:
-            result = super().detect_and_ocr(*args, **kwargs)
-        finally:
-
-            self.model.text_recognizer = text_recognizer
-            self.model.detect_and_ocr = detect_and_ocr
-        return result
-
-
-class StoneOcr(VerticalText):
+class StoneOcr(BaseCor):
     def pre_process(self, image):
         yuv = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
         _, u, _ = cv2.split(yuv)
@@ -74,14 +31,12 @@ class StoneOcr(VerticalText):
         cv2.convertScaleAbs(image, alpha=3, dst=image)
         cv2.subtract((255, 255, 255, 0), image, dst=image)
 
-        # from PIL import Image
-        # Image.fromarray(image.astype(np.uint8)).show()
         return image
 
 
 if __name__ == '__main__':
     from tasks.SixRealms.assets import SixRealmsAssets
-    file = r'C:\Users\Ryland\Desktop\Desktop\20.png'
+    file = r'E:\img_tmp\MuMu12-20250722-235421.png'
     image = load_image(file)
     ocr = StoneOcr(roi=(0,0,1280,720), area=(0,0,1280,720), mode="Full", method="Default", keyword="", name="ocr_map")
     results = ocr.detect_and_ocr(image)
