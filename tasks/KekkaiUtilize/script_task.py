026c9fa0781ad3d9142fa508b54f882a3368ac68
Migs <migsmiku@gmail.com>
2026-01-05 04:52:23 +0800

    fix(Consignment): Rearrange UI click and add comments for clarity

--- tasks/KekkaiUtilize/script_task.py
+++ tasks/KekkaiUtilize/script_task.py
@@ -815,7 +815,8 @@ class ScriptTask(GameUi, ReplaceShikigami, KekkaiUtilizeAssets):
                 break
             if self.appear_then_click(self.I_PLANT_TREE_CLOSE):
                 continue
-
+            if self.appear_then_click(self.I_UI_BACK_BLUE, interval=1):
+                continue
             if self.appear_then_click(self.I_UI_BACK_RED, interval=1):
                 continue
             if self.appear_then_click(self.I_UI_BACK_YELLOW, interval=1):
