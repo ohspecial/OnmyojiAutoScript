c7af2dab70f28dc0788094fbcff20c2b415052e0
Asriel Xu <42128862+xxz7528@users.noreply.github.com>
2026-02-08 09:49:13 +0800

    修复探索任务结束时触发宝箱/妖气导致卡死的问题
    现有的quit_explore函数仅考虑了退出探索回到入口界面的情况。然而，当探索任务结束时，该次探索任务可能触发宝箱、妖气、石距等特殊任务，这会导致此时界面会变为探索大世界界面。
    此时quit_explore会超时强制点左上角，从而触发回到主界面-点击头像-卡死的过程。
    本pull request修改了这一函数以解决该问题，实测有效。

--- tasks/Exploration/base.py
+++ tasks/Exploration/base.py
@@ -348,16 +348,37 @@ class BaseExploration(GameUi, GeneralBattle, GeneralRoom, GeneralInvite, Replace
         logger.info('Quit explore')
         boss_timer = Timer(15)
         boss_timer.start()
+        click_yellow_button = 0 #用于保证只点一次左上返回按钮，不要直接触发连点回到主界面
+        
         while 1:
             self.screenshot()
+            
+            #探索章节标题界面
             if self.appear(self.I_UI_BACK_RED) and self.appear(self.I_E_EXPLORATION_CLICK):
                 break
+            
+            #探索大世界界面
+            if self.appear(self.I_CHECK_EXPLORATION) and not self.appear(self.I_E_SETTINGS_BUTTON):
+                break
+  
+            # 防止BOSS打完箱子刚落地，脚本就手快点退出了
+            if self.appear_then_click(self.I_BATTLE_REWARD, interval=1.5):
+                logger.info("Found battle reward during exit, picking it up.")
+                boss_timer.reset()
+                continue
+
             if boss_timer.reached():
                 # https://github.com/runhey/OnmyojiAutoScript/issues/548
                 logger.warning('Exit immediately after the boss battle')
                 break
             if self.appear_then_click(self.I_E_EXIT_CONFIRM, interval=0.8):
                 continue
+            
+            if click_yellow_button == 0:
+                if self.appear_then_click(self.I_BACK_YOLLOW, interval=3.5):
+                    click_yellow_button = 1
+                    continue
+            
             if self.appear(self.I_EXPLORATION_TITLE) or self.appear(self.I_CHECK_EXPLORATION):
                 continue
             if self.appear_then_click(self.I_UI_BACK_BLUE, interval=3.5):
