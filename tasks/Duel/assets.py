22b609a9ef15f29f285145c257ca71af16574e2c
Azur <azur.tian@qq.com>
2026-02-10 21:29:10 +0800

    fix(Duel): 优化斗技任务配置和界面识别
    - 调整了ActivityShikigami任务中的随机点击区域坐标
    - 移除了Duel任务中废弃的角色切换相关图片资源
    - 更新了斗技界面各种元素的识别区域坐标
    - 将阴阳师枚举类型移动到统一配置模块
    - 重构了斗技任务主流程，优化了战斗准备和执行逻辑
    - 添加了名仕模式检测和荣誉分数OCR识别坐标调整
    - 优化了斗技界面检测和退出机制

--- tasks/Duel/assets.py
+++ tasks/Duel/assets.py
@@ -39,9 +39,9 @@ class DuelAssets:
 	# 战 
 	I_D_WORD_BATTLE = RuleImage(roi_front=(613,41,54,54), roi_back=(600,24,84,85), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_d_word_battle.png")
 	# 名士的星星 
-	I_D_CELEB_STAR = RuleImage(roi_front=(586,456,30,32), roi_back=(574,444,53,54), threshold=0.75, method="Template matching", file="./tasks/Duel/duel/duel_d_celeb_star.png")
+	I_D_CELEB_STAR = RuleImage(roi_front=(646,450,33,37), roi_back=(551,433,185,67), threshold=0.75, method="Template matching", file="./tasks/Duel/duel/duel_d_celeb_star.png")
 	# 名士专属 
-	I_D_CELEB_HONOR = RuleImage(roi_front=(665,531,25,30), roi_back=(652,517,52,53), threshold=0.75, method="Template matching", file="./tasks/Duel/duel/duel_d_celeb_honor.png")
+	I_D_CELEB_HONOR = RuleImage(roi_front=(338,641,25,30), roi_back=(199,606,222,76), threshold=0.75, method="Template matching", file="./tasks/Duel/duel/duel_d_celeb_honor.png")
 	# 练 
 	I_BATTLE_WITH_TRAIN = RuleImage(roi_front=(1161,588,78,81), roi_back=(1114,547,163,172), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_battle_with_train.png")
 	# 练2 
@@ -51,16 +51,18 @@ class DuelAssets:
 	# 活动的战斗按钮 
 	I_D_BATTLE2 = RuleImage(roi_front=(1171,592,69,78), roi_back=(1119,566,154,143), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_d_battle2.png")
 	# 禁用 
-	I_BAN = RuleImage(roi_front=(1112,537,100,100), roi_back=(1112,537,100,100), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_ban.png")
+	I_BAN = RuleImage(roi_front=(1139,565,77,72), roi_back=(1122,545,114,116), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_ban.png")
 	# 左上角的退出 
-	I_DUEL_EXIT = RuleImage(roi_front=(28,11,38,36), roi_back=(4,1,100,71), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_duel_exit.png")
-	# description 
-	I_SHARE_BUTTON = RuleImage(roi_front=(1146,626,52,45), roi_back=(1146,626,52,45), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_share_button.png")
+	I_DUEL_EXIT = RuleImage(roi_front=(14,12,43,41), roi_back=(14,12,43,41), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_duel_exit.png")
+	# ban选界面 
+	I_D_CHECK_BAN = RuleImage(roi_front=(611,36,56,62), roi_back=(590,14,100,100), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_d_check_ban.png")
+	# 斗技荣誉图标 
+	I_DUEL_HONOR = RuleImage(roi_front=(211,636,20,20), roi_back=(200,601,130,100), threshold=0.8, method="Template matching", file="./tasks/Duel/duel/duel_duel_honor.png")
 
 
 	# Ocr Rule Assets
 	# 荣誉 
-	O_D_HONOR = RuleOcr(roi=(149,665,105,24), area=(149,665,105,24), mode="DigitCounter", method="Default", keyword="", name="d_honor")
+	O_D_HONOR = RuleOcr(roi=(233,644,131,31), area=(233,643,131,31), mode="DigitCounter", method="Default", keyword="", name="d_honor")
 	# 分数 
 	O_D_SCORE = RuleOcr(roi=(604,484,108,28), area=(604,484,108,28), mode="DigitCounter", method="Default", keyword="", name="d_score")
 	# 名士星星数 
--- tasks/Duel/script_task.py
+++ tasks/Duel/script_task.py
@@ -13,7 +13,7 @@ from module.base.timer import Timer
 from tasks.Component.GeneralBattle.general_battle import GeneralBattle
 from tasks.Component.SwitchOnmyoji.switch_onmyoji import SwitchOnmyoji
 from tasks.GameUi.game_ui import GameUi
-from tasks.GameUi.page import page_duel, page_onmyodo, random_click, page_reward, page_failed
+from tasks.GameUi.page import page_duel, page_onmyodo, random_click
 from tasks.Duel.config import Duel
 from tasks.Duel.assets import DuelAssets
 from tasks.Component.SwitchSoul.switch_soul import SwitchSoul
@@ -41,51 +41,30 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
         self.limit_time: timedelta = timedelta(hours=limit_time.hour, minutes=limit_time.minute,
                                                seconds=limit_time.second)
         self.prepare_duel()
-        self.current_score = self.conf.duel_celeb_config.initial_score
-        duel_week_over = False
-        while 1:
-            # 任务执行时间超过限制时间，退出
-            if datetime.now() - self.start_time >= self.limit_time:
-                logger.info('Duel task is over time')
-                break
+        while True:
             self.screenshot()
             self.check_and_get_reward()
             if not self.duel_main():
                 self.ui_goto_page(page_duel)
                 continue
-            # 当前分数跟目标分数比较, 判断分数是否已经满足条件
-            if self.get_and_update_cur_score() >= self.conf.duel_config.target_score:
-                logger.info('Duel task is over score')
-                break
-            # 若不开启名仕战斗, 则到达名士直接退出
-            if not self.conf.duel_celeb_config.celeb_battle and self.is_celeb:
-                logger.info('You are already a celeb（名仕）')
-                duel_week_over = True
-                break
-            # 练习
-            if self.appear(self.I_BATTLE_WITH_TRAIN) or self.appear(self.I_BATTLE_WITH_TRAIN2):
-                break
-            # 荣誉满了，退出
-            if self.conf.duel_config.honor_full_exit and self.check_honor():
-                logger.info('Duel task is over honor')
+            if not self.can_start_duel():
                 break
             self.start_duel()
         logger.info('Duel battle end')
+        # 记得退回去到町中
+        self.ui_click(self.I_UI_BACK_YELLOW, self.I_CHECK_TOWN)
+
         if duel_week_over:
             self.set_next_run(task='Duel', success=True, finish=True)
         else:
             self.set_next_run(task='Duel', success=True, finish=False)
-        self.ui_goto_page(page_main)
+
         # 调起花合战
         self.set_next_run(task='TalismanPass', target=datetime.now())
         raise TaskEnd('Duel')
 
     def prepare_duel(self):
         """斗技准备工作(切换御魂or阴阳师...), 最后回到斗技主界面"""
-        page_reward.links.clear()
-        page_failed.links.clear()
-        page_reward.link(button=random_click(ltrb=(True, True, False, True)), destination=page_duel)
-        page_failed.link(button=random_click(ltrb=(True, True, False, True)), destination=page_duel)
         self.ui_goto_page(page_main)
         self.switch_soul()
         if self.conf.duel_config.switch_enabled:
@@ -93,12 +72,36 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
             self.switch_onmyoji(self.conf.duel_config.switch_onmyoji)
         self.ui_goto_page(page_duel)
         self.switch_all_soul()
+        self.current_score = self.conf.duel_celeb_config.initial_score
+
+    def can_start_duel(self) -> bool:
+        """是否可以运行斗技"""
+        # 任务执行时间超过限制时间，退出
+        if datetime.now() - self.start_time >= self.limit_time:
+            logger.info('Duel task is over time')
+            return False
+        # 当前分数跟目标分数比较, 判断分数是否已经满足条件
+        if self.get_and_update_cur_score() >= self.conf.duel_config.target_score:
+            logger.info('Duel task is over score')
+            return False
+        # 若不开启名仕战斗, 则到达名士直接退出
+        if not self.conf.duel_celeb_config.celeb_battle and self.is_celeb:
+            logger.info('You are already a celeb（名仕）')
+            return False
+        # 练习
+        if self.appear(self.I_BATTLE_WITH_TRAIN) or self.appear(self.I_BATTLE_WITH_TRAIN2):
+            return False
+        # 荣誉满了，退出
+        if self.conf.duel_config.honor_full_exit and self.check_honor():
+            logger.info('Duel task is over honor')
+            return False
+        return True
 
     def start_duel(self):
         """进行一次斗技"""
         logger.hr('Duel battle', 2)
         self.current_count += 1
-        self.start_battle()
+        self.enter_battle()
         self.battle_prepare()
         battle_ret = self.wait_battle()
         if battle_ret:
@@ -113,8 +116,8 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
         logger.info(f'battle time: {task_run_time_seconds} / {self.limit_time}')
         self.ui_goto_page(page_duel)
 
-    def start_battle(self):
-        """点击开始战斗"""
+    def enter_battle(self):
+        """点击开始战斗(一直到出现战斗准备界面)"""
         logger.hr('duel battle matching')
         while not self.is_in_battle_prepare():
             self.screenshot()
@@ -143,7 +146,8 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
             if self.appear_then_click(self.I_BAN, interval=1.2):
                 self.is_celeb = True
                 continue
-            if self.is_celeb:  # 名仕不开启自动上阵, 根据最后一个式神的名字是否改变来检查自己式神是否被ban
+            # 名仕不开启自动上阵, 根据最后一个式神的名字是否改变来检查自己式神是否被ban
+            if not self.appear(self.I_D_CHECK_BAN, interval=0.8) and self.is_celeb:
                 ocr_name = self.O_D_BAN_NAME.ocr(self.device.image)
                 shikigami_banned = ocr_name != '' and not any(
                     char in ocr_name for char in self.conf.duel_celeb_config.ban_name)
@@ -164,7 +168,7 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
         logger.hr('duel battle waiting')
         battle_operated = False
         battle_timeout_timer = Timer(270).start()
-        ret_timer = Timer(2.5)
+        ret_timer = Timer(5)
         battle_timeout_cnt, max_timeout_cnt = 0, 3
         ret = None
         while True:
@@ -179,11 +183,6 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
                 self.ui_goto_page(page_duel)
                 break
             if self.is_battle_win():
-                ret = True
-                ret_timer.start()
-                self.click(random_click(ltrb=(True, True, False, True)), interval=1.2)
-                continue
-            if self.appear(self.I_SHARE_BUTTON, interval=0.6):
                 ret = True
                 ret_timer.start()
                 self.click(random_click(ltrb=(True, True, False, True)), interval=1.2)
@@ -222,10 +221,15 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
 
     def check_honor(self) -> bool:
         """检查荣誉是否满了"""
+        if not self.appear(self.I_DUEL_HONOR):
+            return False
+        roi_x = self.I_DUEL_HONOR.roi_front[0] + self.I_DUEL_HONOR.roi_front[2]
+        roi_y = self.I_DUEL_HONOR.roi_front[1]
+        roi_w = 110
+        roi_h = self.I_DUEL_HONOR.roi_front[3]
+        self.O_D_HONOR.roi = [roi_x, roi_y, roi_w, roi_h]
         current, remain, total = self.O_D_HONOR.ocr(self.device.image)
-        if current == total and remain == 0:
-            return True
-        return False
+        return current == total and remain == 0
 
     def get_and_update_cur_score(self, skip_screenshot: bool = True) -> int:
         """
@@ -283,13 +287,26 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
                 break
             if self.appear_then_click(self.I_D_TEAM, interval=1):
                 continue
-            if self.appear_then_click(self.I_UI_CONFIRM, interval=0.6):
+            not_in_prepare_cnt = 0
+            # 再次检查是否是名仕(若斗技主界面识别名仕失效的话)
+            if self.appear_then_click(self.I_BAN, interval=1.2):
+                self.is_celeb = True
                 continue
-            if self.appear_then_click(self.I_D_TEAM_SWTICH, interval=1):
-                click_count += 1
+            if self.is_celeb:  # 名仕不开启自动上阵, 根据最后一个式神的名字是否改变来检查自己式神是否被ban
+                ocr_name = self.O_D_BAN_NAME.ocr(self.device.image)
+                shikigami_banned = ocr_name != '' and not any(
+                    char in ocr_name for char in self.conf.duel_celeb_config.ban_name)
+                logger.info(f'Check self shikigami is banned:{shikigami_banned}')
+                if shikigami_banned:
+                    self.duel_exit_battle()
+                    continue
+                self.click(self.C_DUEL_CLICK_5, interval=random.uniform(0.7, 1.4))
+                sleep(random.uniform(1.5, 3))  # 降低点击频率和ocr识别频率
                 continue
-        logger.info('Souls Switch is complete')
-        self.ui_click(self.I_UI_BACK_YELLOW, self.I_D_TEAM)
+            # 点击自动上阵或准备
+            if self.appear_then_click(self.I_D_AUTO_ENTRY, interval=1.2) or \
+                    self.appear_then_click(self.I_D_PREPARE, interval=1.2):
+                self.reset_device('PREPARE_BEFORE_BATTLE')
 
     def check_and_get_reward(self):
         """检查并收获奖励"""
@@ -303,7 +320,8 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
         return self.appear(self.I_D_PREPARE) or \
             self.appear(self.I_D_AUTO_ENTRY) or \
             self.appear(self.I_BAN) or \
-            self.appear(self.I_D_WORD_BATTLE)
+            self.appear(self.I_D_WORD_BATTLE) or \
+            self.appear(self.I_D_CHECK_BAN)
 
     def is_battle_win(self) -> bool:
         return self.appear(self.I_WIN) or self.appear(self.I_D_VICTORY)
@@ -324,8 +342,7 @@ class ScriptTask(GameUi, GeneralBattle, SwitchSoul, DuelAssets, SwitchOnmyoji):
 if __name__ == '__main__':
     from module.config.config import Config
     from module.device.device import Device
-
-    c = Config('zhu')
+    c = Config('oas2')
     d = Device(c)
     t = ScriptTask(c, d)
 
