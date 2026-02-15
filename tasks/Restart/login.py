3ca03b602a919e8845d1dd02ad1f7c37aa86195c
yuyuyuyuyu <190754820@qq.com>
2026-02-15 17:42:33 +0800

    feat(Restart): 重启添加庭院事务  -添加庭院事务  -为ui_click方法添加超时时间参数  -添加ui_click_multi_scalef方法

--- tasks/Restart/login.py
+++ tasks/Restart/login.py
@@ -318,28 +318,8 @@ class LoginHandler(BaseTask, RestartAssets, GameUiAssets):
                 continue
         self.ui_click_until_disappear(self.I_LOGIN_RED_CLOSE)
         return True
-        
+            
     def harvest_courtyard_affairs(self) -> bool:
-        if not self.ui_click_multi_scale(self.I_NOTE, self.I_PAGE, timeout=3, scale_range=(0.8, 1.2)):
-            logger.warning('courtyard affairs timeout!')
-            return False
-        while 1:
-            self.screenshot()
-            if self.appear(self.I_NO_TASKS):
-                logger.info('courtyard affairs completed！')
-                return True
-            if self.appear_then_click(self.I_COMPLETE_TASKS, interval=1):
-                continue
-            if self.appear_then_click(self.I_DAILY, interval=1):
-                continue
-            if self.appear_then_click(self.I_CONFIRM): #式神满级，是否提取物经验？确定
-                continue
-            if self.appear_then_click(self.I_SUCCESS_CLAIMED):
-                continue
-            if self.appear_then_click(self.I_SKIP):# 万花牌跳过
-                continue
-            if self.appear_then_click(self.I_LOGIN_RED_CLOSE):# 万花牌X
-                continue    def harvest_courtyard_affairs(self) -> bool:
         if not self.ui_click_multi_scale(self.I_NOTE, self.I_PAGE, timeout=3, scale_range=(0.8, 1.2)):
             logger.warning('courtyard affairs timeout!')
             return False
