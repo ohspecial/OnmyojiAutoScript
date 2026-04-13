cf8be3626218f87a43d4d19d9f99f79d4ee3affb
ohspecial <105205583+ohspecial@users.noreply.github.com>
2026-04-13 23:57:51 +0800

    fix SixRealms

--- tasks/SixRealms/oas_ocr.py
+++ tasks/SixRealms/oas_ocr.py
@@ -23,49 +23,8 @@ def apply_mask(image, mask):
     return image16.astype(np.uint8)
 
 
-class VerticalText(BaseCor):
-    @staticmethod
-    def rotate_image(image):
-        height, width = image.shape[0:2]
-        if height * 1.0 / width >= 1.5:
-            image = np.rot90(image)
-            return image
-        return image
 
-    def detect_and_ocr(self, *args, **kwargs):
-        if getattr(self.model, "is_proxy", False):
-            params = {"drop_score": 0.1, "box_thresh": 0.2, "vertical": True}
-            for key in list(params.keys()):
-                if key in kwargs:
-                    params.pop(key)
-            return self.model.detect_and_ocr(*args, **params, **kwargs)
-        # Try hard to lower TextSystem.box_thresh
-        backup = self.model.text_detector.box_thresh
-        # Patch text_recognizer
-        text_recognizer = self.model.text_recognizer
-        # Lower drop_score
-        lower_score = functools.partial(self.model.detect_and_ocr, drop_score=0.1)
-        detect_and_ocr = self.model.detect_and_ocr
-
-        def vertical_text_recognizer(img_crop_list):
-            img_crop_list = [VerticalText.rotate_image(i) for i in img_crop_list]
-            result = text_recognizer(img_crop_list)
-            return result
-
-        self.model.text_detector.box_thresh = 0.2
-        self.model.text_recognizer = vertical_text_recognizer
-        self.model.detect_and_ocr = lower_score
-
-        try:
-            result = super().detect_and_ocr(*args, **kwargs)
-        finally:
-            self.model.text_detector.box_thresh = backup
-            self.model.text_recognizer = text_recognizer
-            self.model.detect_and_ocr = detect_and_ocr
-        return result
-
-
-class StoneOcr(VerticalText):
+class StoneOcr(BaseCor):
     def pre_process(self, image):
         yuv = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
         _, u, _ = cv2.split(yuv)
