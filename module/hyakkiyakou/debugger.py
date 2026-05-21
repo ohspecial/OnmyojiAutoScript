42a57b80c46d648cdd47c2ddc9faa3c719776f0d
ohspecial <q459278554@gmail.com>
2026-05-21 11:01:16 +0800

    调整百鬼夜行回馈学习保存逻辑

--- module/hyakkiyakou/debugger.py
+++ module/hyakkiyakou/debugger.py
@@ -144,13 +144,8 @@ class Debugger:
         return _class in self.save_class
 
     def deal_learning(self, image, tracks: list):
-        save_flag: bool = False
-        for _id, _class, _conf, _cx, _cy, _w, _h, _v in tracks:
-            if self.check_class(_class):
-                save_flag = True
-                break
-        if not save_flag:
-            return
+        # Continuous learning is used to collect training data, including
+        # missed/low-confidence targets. Save every sampled battle frame.
         time_now_image_name = f'hya_{int(time.time() * 1000)}'
         self.images_cache[time_now_image_name] = image
 
