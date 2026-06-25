967d17382a36d72fd5d2f1703c95e9ff59a7a08d
ohspecial <q459278554@gmail.com>
2026-06-25 09:30:12 +0800

    fix: return to courtyard after memory scrolls check

--- tasks/MemoryScrolls/script_task.py
+++ tasks/MemoryScrolls/script_task.py
@@ -9,7 +9,7 @@ from module.base.timer import Timer
 from datetime import timedelta, datetime
 
 from tasks.GameUi.game_ui import GameUi
-from tasks.GameUi.page import page_summon
+from tasks.GameUi.page import page_main, page_summon
 from tasks.MemoryScrolls.assets import MemoryScrollsAssets
 from tasks.MemoryScrolls.config import ScrollNumber
 
@@ -21,6 +21,7 @@ class ScriptTask(GameUi, MemoryScrollsAssets):
         con = self.config.memory_scrolls.memory_scrolls_config
         # 进入绘卷主界面
         self.goto_memoryscrolls_main(con) 
+        self.goto_page(page_main)
         raise TaskEnd
     
     def goto_memoryscrolls_main(self, con):
@@ -161,7 +162,6 @@ if __name__ == '__main__':
     t.screenshot()
 
     t.run()
-
 
 
 
